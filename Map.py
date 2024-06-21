cd454e31324c32d18939329ba058f48ab8828f94
nhu1207 <66822864+nhu1207@users.noreply.github.com>
2024-06-21 14:15:44 +0900

    Update Map.py

--- Map.py
+++ Map.py
@@ -160,7 +160,19 @@ class Map:
     # A attackablility check that consider a range not only a movepoint.
     # check whether is id1 able to attack id2.
     def is_movable_with_range(self, id1, id2, movement_point, range):
-        
+        """
+        A attackablility check that consider a range not only a movepoint.
+        check whether is id1 able to attack id2.
+
+        Args:
+            id1: subject or caster (attacker/mover)
+            id2: object
+            movement_point: movement point of id1
+            range: range of attack
+
+        Returns:
+            bool
+        """
         return self.distance(id1, id2) < (movement_point + range)
 
     # 어택땅
