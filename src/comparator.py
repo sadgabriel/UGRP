89be29e75e1fee273fd883a1a4e0a2d062b45291
nhu1207 <66822864+nhu1207@users.noreply.github.com>
2024-07-30 16:53:32 +0900

    comparator.py complete
    Complete: comparator.py
    Add: data sample
    (
        preprocessed\batch0.jason
        and
        compared\batch0.json
    )

--- src/comparator.py
+++ src/comparator.py
@@ -1,11 +1,12 @@
 from labeler import estimate
 from labeler import output_parameters_name
 from labeler import load_folder
+from labeler import save_folder
 import json
 import os
 
 
-def compare(file_num: int = 100, data_num: int = 100) -> None:
+def compare(file_num: int = 100) -> None:
     """
     Load labelled data and preprocessed data.
     Estimate preprocessed data.
@@ -22,38 +23,39 @@ def compare(file_num: int = 100, data_num: int = 100) -> None:
     # load preprocessed data. They don't have estimated parameters.
     preprocessed_data = load_folder(path="..\\data\\preprocessed", file_num=file_num)
 
-    # Make scores of preprocessed data by estimating preprocessed_data.
-    preprocessed_scores = list()
-    for _100_levels in preprocessed_data:
-        preprocessed_scores.append(list())
-        cur = len(preprocessed_scores) - 1
-        for level in _100_levels:
-            preprocessed_scores[cur].appned(estimate(level))
+    # Update parameters of preprocessed data by estimating preprocessed_data.
+    for i in range(len(preprocessed_data)):
+        map_list = preprocessed_data[i]["map_list"]
+        data_num = len(map_list)
+        for j in range(data_num):
+            new_params = estimate(map_list[j]["map"])
+            preprocessed_data[i]["map_list"][j]["params"].update(new_params)
 
     # Make a list that it contains parameters from two datasets.
-    result = list()
+    compared_data = list()
 
     for i in range(file_num):
-        result.append(list())
-        cur = i - 1
+        temp_map_list = {"map_list": []}
+        compared_data.append(temp_map_list)
+
+        labelled_map_list = labelled_data[i]["map_list"]
+        preprocessed_map_list = preprocessed_data[i]["map_list"]
 
         for j in range(data_num):
-            temp = dict()
-            for param_name in output_parameters_name:
-                temp[param_name] = tuple(
-                    labelled_data[i][j][param_name],
-                    preprocessed_scores[i][i][param_name],
-                )
-            result[cur].append(temp)
+            temp_compared_data = {"before_params": {}, "after_params": {}}
+            compared_data[i]["map_list"].append(temp_compared_data)
+
+            compared_data[i]["map_list"][j]["before_params"] = labelled_map_list[j][
+                "params"
+            ]
+            compared_data[i]["map_list"][j]["after_params"] = preprocessed_map_list[j][
+                "params"
+            ]
 
     # Save the result list.
-    save_folder(data=result)
+    save_folder(data=compared_data, path="..\\data\\compared", file_num=file_num)
 
     return
-
-
-def save_folder(data, path: str = "..\\data\\compared") -> None:
-    pass
 
 
 if __name__ == "__main__":
