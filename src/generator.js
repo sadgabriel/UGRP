2b382fd30c3e2b8ba8541150498a32ca678fa482
MintGhost <sadgabriel@protonmail.com>
2024-08-01 17:04:28 +0900

    implement generator.js

--- src/generator.js
+++ src/generator.js
@@ -1,69 +1,162 @@
 #!/usr/bin/env node
 
-/**
- * 의사코드
- * def createMap():
- *  여러 매개 변수를 받아 맵 객체를 반환한다.
- *  보다 직접적인 파라미터를 받는다. 
- * 
- * def createMaps():
- *  매개 변수를 받아 맵 객체의 리스트를 반환한다.
- *  보다 추상적인 파라미터를 받는다.
- *  파일 최상단의 상수 부분에 있는 값들을 가져와서 쓴다. 각 맵 크기에 따른 설정들.
- *  작은 맵, 중간 맵, 큰 맵을 만드는 과정을 거친다. 만드는 건 createMap으로 만든다.
- * 
- * def saveMaps():
- *  맵의 리스트와 저장될 경로를 받아 맵을 저장한다.
- * 
- * def convertToString():
- *  node roguelike가 만든 맵을 문자열 형태로 바꿔서 반환한다.
- */
+const SMALL_MIN = 9;
+const SMALL_MAX = 16;
+const SMALL_ROOM_MIN = 2;
+const SMALL_ROOM_MAX = 7;
+const SMALL_ROOM_IDEAL = 10;
+const SMALL_RETRY = 100;
+
+const LARGE_MIN = 16;
+const LARGE_MAX = 30;
+const LARGE_ROOM_MIN = 3;
+const LARGE_ROOM_MAX = 9;
+const LARGE_ROOM_IDEAL = 10;
+const LARGE_RETRY = 100;
+
 
 const fs = require('fs')
-var roguelike = require('../roguelike/node_modules/roguelike/level/roguelike');
+const path = require('path')
+var roguelike = require(path.join(__dirname, "node-roguelike", "level", "roguelike"));
 
-const fileStream = fs.createWriteStream('input/test_map3.txt');
-
-var level = roguelike({
-  width: 31,
-  height: 19,
-  retry: 100,
-  special: false,
-  room: {
-    ideal: 11,
-    min_width: 3,
-    max_width: 9,
-    min_height: 3,
-    max_height: 9
-  }
-});
-
-var world = level.world;
-
-// Crude mechanism for drawing level
-for (var y = 0; y < world.length; y++) {
-  var row = '';
-  for (var x = 0; x < world[y].length; x++) {
-    var tile = world[y][x];
-    if (tile === 0) {
-      row += ' ';
-    } else if (tile === 1) {
-      row += '.';
-    } else if (tile === 2) {
-      row += '#';
-    } else if (tile === 3) {
-      row += '/';
-    } else if (tile === 4) {
-      row += 'X';
-    } else if (tile === 5) {
-      row += '<';
-    } else if (tile === 6) {
-      row += '>';
-    } else {
-      row += world[y][x];
-    }
-  }
-
-  fileStream.write(row + '\n');
+function getRandomInt(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
+function divideArray(array, batchSize) {
+  let result = [];
+  for (let i = 0; i < array.length; i += batchSize) {
+    let batch = array.slice(i, i + batchSize);
+    result.push(batch);
+  }
+  return result;
+}
+
+function convertToString(level){
+  let world = level.world;
+
+  let stringMap = "";
+
+  const tileMapping = {
+    0: ' ',
+    1: '.',
+    2: '#',
+    3: '/',
+    4: 'X',
+    5: '<',
+    6: '>'
+  };
+
+  for (let y = 0; y < world.length; y++) {
+    let row = '';
+    for (let x = 0; x < world[y].length; x++) {
+      let tile = world[y][x];
+      row += tileMapping[tile] || tile;
+    }
+    stringMap += row + '\n';
+  }
+
+  return stringMap;
+}
+
+function createMap(width, height, roomMinWidth, roomMaxWidth, roomMinHeight, roomMaxHeight, roomIdeal, retry){
+
+  let level = roguelike({
+    width: width,
+    height: height,
+    retry: retry,
+    special: false,
+    room: {
+      ideal: roomIdeal,
+      min_width: roomMinWidth,
+      max_width: roomMaxWidth,
+      min_height: roomMinHeight,
+      max_height: roomMaxHeight
+    }
+  });
+
+  let stringMap = convertToString(level);
+
+  let map = {
+    params: {
+      map_width: width,
+      map_height: height,
+      retry: retry,
+      room_ideal: roomIdeal,
+      room_min_width: roomMinWidth,
+      room_max_width: roomMaxWidth,
+      room_min_height: roomMinHeight,
+      room_max_height: roomMaxHeight
+    },
+    map: stringMap
+  }
+
+  return map;
+}
+
+function createMaps(small, large){
+  let maps = [];
+
+  let error_count = 0;
+  for (let i = 0; i < small; i++){
+    let width = getRandomInt(SMALL_MIN, SMALL_MAX);
+    let height = getRandomInt(SMALL_MIN, SMALL_MAX);
+    
+    try {
+      var map = createMap(width, height, SMALL_ROOM_MIN, SMALL_ROOM_MAX, SMALL_ROOM_MIN, SMALL_ROOM_MAX, SMALL_ROOM_IDEAL, SMALL_RETRY);
+    } catch {
+      error_count++;
+      if (error_count > 100){
+        return maps;
+      }
+      i--;
+      continue;
+    }
+
+    maps.push(map);
+  }
+
+  for (let i = 0; i < large; i++){
+    let width = getRandomInt(LARGE_MIN, LARGE_MAX);
+    let height = getRandomInt(LARGE_MIN, LARGE_MAX);
+
+    try {
+      var map = createMap(width, height, LARGE_ROOM_MIN, LARGE_ROOM_MAX, LARGE_ROOM_MIN, LARGE_ROOM_MAX, LARGE_ROOM_IDEAL, LARGE_RETRY);
+    } catch {
+      error_count++;
+      if (error_count > 100){
+        return maps;
+      }
+      i--;
+      continue;
+    }
+    maps.push(map);
+  }
+
+  return maps;
+}
+
+function saveMaps(maps, prefix = "batch", batchSize = 100){
+  batches = divideArray(maps, batchSize);
+  let directoryName = path.join(path.dirname(__dirname), "data", "raw");
+
+  if (!fs.existsSync(directoryName)) {
+    fs.mkdirSync(directoryName, { recursive: true });
+  }
+  
+  for (let i = 0; i < batches.length; i++){
+    let jsonData =  JSON.stringify({"map_list": batches[i]}, null, 4);
+    fs.writeFile(path.join(directoryName, `${prefix}${i}.json`), jsonData, (err) => {
+      if (err) {
+        console.error('An error occurred:', err);
+        return;
+      }
+      console.log('JSON file has been saved.');
+    });
+  }
+}
+
+let maps = createMaps(100, 100);
+saveMaps(maps);
