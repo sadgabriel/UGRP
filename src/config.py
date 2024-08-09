bbb75f708a4404aa267523d9bebf6075c221d05f
afterrest <afterrest00@gmail.com>
2024-08-09 16:16:58 +0900

    change as config

--- src/config.py
+++ src/config.py
@@ -1,8 +1,12 @@
 PROMPT_STYLE = "AutoCOT1"
-EXAMPLE_COUNT = 10
-PARAMS = {"MAP_SIZE": [20, 20], "ROOM_COUNT": 7, "ENEMY_COUNT": 2, "TREASURE_COUNT": 1}
 
-BASE_PROMPT_FILE_PATH = f"../data/4. prompt/base_prompt/{PROMPT_STYLE}.txt"
-EXAMPLE_PATH = f"../data/3. labelled"
-PROMPT_FILE_PATH = f"../data/4. prompt/{PROMPT_STYLE}.txt"
-PREPROCESSED_DATA_FILE_PATH = f"../data/5. preprocessed/{PROMPT_STYLE}.txt"
+
+RAW_PATH = "../data/1. raw/"
+PLACED_PATH = "../data/2. placed/"
+LABELED_PATH = "../data/3. labelled/"
+PROMPT_PATH = "../data/4. prompt/"
+PREPROCESSED_PATH = "../data/5. preprocessed/"
+COMPARED_PATH = "../data/6. compared/"
+
+BASE_PROMPT_FILE_PATH = f"./base_prompt/{PROMPT_STYLE}.txt"
+PROMPT_FILE_PATH = PROMPT_PATH + f"{PROMPT_STYLE}.txt"
