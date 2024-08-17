31fae981c3265cbbebe3646af2848acbf9b471df
afterrest <afterrest00@gmail.com>
2024-08-17 15:28:33 +0900

    param generator

--- src/param_generator.py
+++ src/param_generator.py
@@ -1,14 +1,20 @@
 import random
-import math
+import numpy as np
 
 config = {
     "small": {
         "map_min": 9,
         "map_max": 16,
+        "room_count": {"mean": 3.245, "std_dev": 1.1510755839648412},
+        "enemy_count": {"mean": 3.7425, "std_dev": 1.9751439820934573},
+        "treasure_count": {"mean": 0.645, "std_dev": 0.4785133226985431},
     },
     "large": {
         "map_min": 16,
         "map_max": 30,
+        "room_count": {"mean": 8.86, "std_dev": 2.6730881018028563},
+        "enemy_count": {"mean": 14.255, "std_dev": 6.732382564887411},
+        "treasure_count": {"mean": 2.255, "std_dev": 0.744966442197231},
     },
     "enemy_density": 0.05,
     "treasure_density": 0.01,
@@ -19,29 +25,20 @@ def generate_param() -> dict:
 
     size = "small" if random.random() < 0.5 else "large"
 
-    map_size_min = config[size]["map_min"]
-    map_size_max = config[size]["map_max"]
+    map_config = config[size]
 
-    map_width = random.randint(map_size_min, map_size_max)
-    map_height = random.randint(map_size_min, map_size_max)
+    map_width = np.random.randint(map_config["map_min"], map_config["map_max"] + 1)
+    map_height = np.random.randint(map_config["map_min"], map_config["map_max"] + 1)
 
-    room_count_max = math.floor((map_width - 1) / 2) * math.floor((map_height - 1) / 2)
-    room_count = random.randint(1, (map_width * map_height) / 25)
+    counts = {"room_count": 0, "enemy_count": 0, "treasure_count": 0}
 
-    empty_space_predict = round(
-        map_width * map_height
-        - (math.sqrt(room_count) - 1) * (map_width + map_height)
-        + (math.sqrt(room_count) - 1) * (math.sqrt(room_count) - 1)
-    )
-    enemy_count = round(empty_space_predict * config["enemy_density"])
-    treasure_count = round(empty_space_predict * config["treasure_density"])
+    counts = {}
+    for key, min_value in zip(
+        ["room_count", "enemy_count", "treasure_count"], [1, 0, 0]
+    ):
+        counts[key] = max(
+            min_value,
+            int(np.random.normal(map_config[key]["mean"], map_config[key]["std_dev"])),
+        )
 
-    return {
-        "map_size": [
-            map_width,
-            map_height,
-        ],
-        "room_count": room_count,
-        "enemy_count": enemy_count,
-        "treasure_count": treasure_count,
-    }
+    return {"map_size": [map_width, map_height], **counts}
